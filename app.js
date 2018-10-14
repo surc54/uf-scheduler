e95ceba1b1a9c734c3417da2187736037e1b931c
Adithya Haridas <adithyah54@gmail.com>
2018-10-14 11:22:31 -0400

    Add structure for scheduler

--- app.js
+++ app.js
@@ -2,7 +2,20 @@ const express = require("express");
 const app = express();
 const http = app.listen(process.env.PORT || 80);
 
+const path = require("path");
+const morgan = require("morgan");
+
+app.use(morgan("dev"));
+
+app.use("/public", express.static("public"));
+
+const schedulerRouter = require("./scheduler/router");
+app.use("/schedule", schedulerRouter);
 
 app.get("/", (req, res) => {
-    res.status(200).send("Good to go.");
+    res.redirect("/schedule");
+});
+
+app.use((req, res) => {
+    res.status(404).send("404?<br>" + req.path);
 });
