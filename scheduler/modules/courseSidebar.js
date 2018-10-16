a5d8983fe4201fdd53d2c2712e03d4a557f0abee
Adithya Haridas <adithyah54@gmail.com>
2018-10-15 22:20:54 -0400

    Move sidebar code to own module

--- scheduler/modules/courseSidebar.js
+++ scheduler/modules/courseSidebar.js
@@ -24,11 +24,12 @@ let CourseSidebar = {
         //     CourseSidebar.resizeCourseList();
         // });
     },
+
     /**
      * Expand Sidebar
      * sidebarAnimationMaxIndex - Controls speed of animation
      */
-    expand: (sidebarAnimationMaxIndex = 50) => {
+    expand: (sidebarAnimationMaxIndex = 25) => {
         $("a#courses-expand").hide();
         let i = 0;
         let interval = setInterval(() => {
@@ -41,26 +42,31 @@ let CourseSidebar = {
                 clearInterval(interval);
             }
             main.css("grid-template-columns", "auto " + (maxValue * percent) + "rem");
-        }, 1);
+        }, 10);
     },
+
     /**
      * Collapse Sidebar
      * sidebarAnimationMaxIndex - Controls speed of animation
      */
-    collapse: (sidebarAnimationMaxIndex = 50) => {
+    collapse: (sidebarAnimationMaxIndex = 25) => {
         $("a#courses-expand").show();
         let i = sidebarAnimationMaxIndex;
+        let initTime = Date.now();
         let interval = setInterval(() => {
             let percent = EasingFunctions.easeOutQuad(i--/sidebarAnimationMaxIndex);
             let maxValue = 50;
             if (percent <= 0) {
                 main.css("grid-template-columns", "");
                 main.addClass("course-collapsed");
+                let endTime = Date.now();
+                console.log("Animation took " + ((endTime - initTime) / 1000) + "s");
                 clearInterval(interval);
             }
             main.css("grid-template-columns", "auto " + (maxValue * percent) + "rem");
-        }, 1);
+        }, 10);
     },
+
     /**
      * Resize Course List
      * Should the size of section.header change, resize courseList to fit on window.
--- scheduler/modules/index.js
+++ scheduler/modules/index.js
@@ -1,15 +1,83 @@
 import $ from "jquery";
-let CourseSidebar = require("./courseSidebar");
+import CourseSidebar from "./CourseSidebar";
 
 let main,
     courses,
-    courseList;
-
+    courseList,
+    courseDrag;
+    
 $(() => {
     main = $("main");
     courses = $("div#courses");
     courseList = courses.find("section#courseList");
+    courseDrag = null;
 
     CourseSidebar.init(main);
+    courseList.find(".course").on("mousedown", (e) => {
+        e.preventDefault();
+        courseDrag = {
+            elem: $(e.currentTarget),
+            activated: false,
+            clickOrigin: {
+                x: e.clientX,
+                y: e.clientY
+            }
+        };
+    });
 
+    $(window).on("mouseup", (e) => {
+        if (courseDrag != null) {
+            if (courseDrag.activated) {
+                e.preventDefault();
+                courseDrag.dragger.removeClass("active");
+                courseDrag.dragger.remove();
+            }
+            courseDrag = null;
+        }
+    });
+
+    $(window).on("mousemove", (e) => {
+        if (courseDrag != null) {
+            let x = e.clientX;
+            let y = e.clientY;
+
+            if (courseDrag.activated) {
+                e.preventDefault();
+                courseDrag.dragger.css({
+                    left: (e.clientX - 0.5 * courseDrag.dragger.width()) + "px",
+                    top: (e.clientY - 0.5 * courseDrag.dragger.height()) + "px"
+                });
+                setTimeout(() => {
+                    if (courseDrag != null && courseDrag.activated) {
+                        courseDrag.dragger.addClass("active");
+                    }
+                }, 100);
+            } else {
+                let dist = distance(x, y, courseDrag.clickOrigin.x, courseDrag.clickOrigin.y);
+                if (dist > 100) {
+                    let cd = document.createElement("div");
+                    cd = $(cd);
+                    cd.addClass("course-drag");
+                    // INSERT CLASS CODE!!
+                    cd.html("COP3503");
+
+                    $("body").append(cd);
+
+                    cd.css({
+                        left: (e.clientX - 0.5 * cd.width()) + "px",
+                        top: (e.clientY - 0.5 * cd.height()) + "px"
+                    });
+
+                    courseDrag.activated = true;
+                    courseDrag.dragger = cd;
+                }
+            }
+        }
+    });
 });
+
+
+
+function distance(x1, y1, x2, y2) {
+    return Math.sqrt(Math.pow(x2-x1, 2) + Math.pow(y2-y1, 2));
+}
