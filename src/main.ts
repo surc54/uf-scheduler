f9e29bdff77965ebd28f34f0d6314f68a6fa814f
Adithya Haridas <34602379+surc54@users.noreply.github.com>
2020-10-01 17:40:17 -0400

    Update to v2/master initial

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,7 @@
-import 'hammerjs';
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { createApp } from 'vue'
+import App from './App.vue'
+import './registerServiceWorker'
+import router from './router'
+import store from './store'
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
-
-if (environment.production) {
-  enableProdMode();
-}
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+createApp(App).use(store).use(router).mount('#app')
