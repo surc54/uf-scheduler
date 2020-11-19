668b347f9ce898e6f69e5f55dbb0f2a61babb78a
Adithya Haridas <34602379+surc54@users.noreply.github.com>
2020-11-19 11:43:14 -0500

    Remove all, in preparation for React

--- src/components/shared/card/card.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="shadow-md py-6 px-4 rounded-md bg-white">
-    <slot></slot>
-  </div>
-</template>
--- src/views/about.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="about flex flex-col justify-center items-center">
-    <h1 class="text-lg font-bold">About page</h1>
-    <p>This page has not been implemented</p>
-    <router-link class="text-indigo-600 hover:text-indigo-900 mt-4" to="/">Go back home</router-link>
-  </div>
-</template>
